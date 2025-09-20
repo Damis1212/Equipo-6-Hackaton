6f50cde4d822743d60d17417ab9cfa5494e29e9b
Lalavanell <lalavanell@gmail.com>
2025-09-19 18:18:22 -0600

    ultimos

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -2,38 +2,40 @@ import { productos } from '../data/productos';
 import { formatCurrency } from '../helpers';
 import useQuiosco from '../hooks/useQuiosco';
 
+export default function Producto({ producto }) {
+    const { handleClickModal, handleSetProducto } = useQuiosco();
+    const { nombre, imagen, descripcionCorta } = producto;
 
-export default function Producto({producto}) {
-
-    const{ handleClickModal, handleSetProducto } = useQuiosco();
-    const { nombre, imagen, descripcionCorta} = producto;
     return (
-    <div className='border p-3 shadow bg-white'>
-        <img 
-            src={`img/${imagen}.jpg`} 
-            className="mx-auto"
-            alt={`Imagen ${nombre}`} 
-        />
-        <div className='p-5'>
-        <h3 className='text-2xl font-bold'>{nombre}</h3>
-        {/* <p className='mt-5 font-black text-4xl text-amber-500'>{formatCurrency(precio)}</p> */}
+        <div className="border p-3 shadow bg-white">
+            <img
+                src={`img/${imagen}.jpg`}
+                className="mx-auto"
+                alt={`Imagen ${nombre}`}
+            />
+            <div className="p-5">
+                <h3 className="text-2xl font-bold">{nombre}</h3>
 
-        <p className="text-gray-600 mt-2 mb-1 text-justify">
-  {descripcionCorta}
-</p>
+                {/* Si quieres mostrar precio, descomenta esto */}
+                {/* <p className="mt-5 font-black text-4xl text-amber-500">
+                    {formatCurrency(precio)}
+                </p> */}
 
-        
-        <button
-            type='button'
-            className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold'
-            onClick={()=> {
-                handleClickModal();
-                handleSetProducto(producto);
-            }}
-        >
-            Agregar Donacion
-        </button>
+                <p className="text-gray-600 mt-2 mb-1 text-justify">
+                    {descripcionCorta}
+                </p>
+
+                <button
+                    type="button"
+                    className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
+                    onClick={() => {
+                        handleClickModal();
+                        handleSetProducto(producto);
+                    }}
+                >
+                    Agregar Donacion
+                </button>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
